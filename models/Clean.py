9cfbe52dd07a86eb5c4de83cef0ff02e2432de94
hasanimran96 <hasanimran_96@yahoo.com>
2019-04-12 20:36:29 +0500

    updated SVMExperiment to now show full statistics

--- models/Clean.py
+++ models/Clean.py
@@ -2,21 +2,21 @@ import pandas as pd
 import numpy as np
 import nltk
 import re
+
 # from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+
 # import math
 
 
-
 class DataCLean:
-
     def extract(self, path):
-        fd = open(path, encoding="utf-8", errors='replace')
+        fd = open(path, encoding="utf-8", errors="replace")
         df = pd.read_csv(fd)
-        defined = df['class'] != ("undefined")
+        defined = df["class"] != ("undefined")
         # #output dataframe without undeined
         df2 = df[defined]
-        defined1 = df2['class'] != "Undefined"
+        defined1 = df2["class"] != "Undefined"
         df4 = df2[defined1]
         # replace no PI with no
         df3 = df4.replace("No PI", "no")
@@ -40,10 +40,10 @@ class DataCLean:
         li_stopwords = stopword[0].split()
         return li_stopwords
 
-
-    def removeStopWords(self,text):
+    def removeStopWords(self, text):
         # stop_words = set(stopwords.words('english'))
-        stop_words = self.read_stopwords("../stopwords.txt")
+        # stop_words = self.read_stopwords("../stopwords.txt")
+        stop_words = self.read_stopwords("stopwords.txt")
         word_tokens = word_tokenize(text)
         filtered_sentence = []
         for w in word_tokens:
@@ -57,21 +57,23 @@ class DataCLean:
         li_stopwords = self.read_stopwords("../stopwords.txt")
         # print(stop_words)
         count_clean = 0
-        for text in df_punc_remove['text']:
+        for text in df_punc_remove["text"]:
             word_tokens = word_tokenize(text)
             clean_text = ""
             for w in word_tokens:
                 if w.lower() not in li_stopwords:
-                    clean_text = clean_text + w.lower() + ' '
-            df_punc_remove.at[count_clean, 'text'] = clean_text
-            df_punc_remove.at[count_clean, 'class'] = df_punc_remove.iloc[count_clean]['class']
+                    clean_text = clean_text + w.lower() + " "
+            df_punc_remove.at[count_clean, "text"] = clean_text
+            df_punc_remove.at[count_clean, "class"] = df_punc_remove.iloc[count_clean][
+                "class"
+            ]
             count_clean += 1
         # return list of corpus without stop words in a list.
         # print(df_punc_remove)
         return df_punc_remove
 
     def removePunc(self, eachText):
-        remove_punc = re.sub(r'[^\w\s]', '', eachText)
+        remove_punc = re.sub(r"[^\w\s]", "", eachText)
         return remove_punc
         # pattern = re.compile(r'[a-zA-Z]+')
         # matches = pattern.finditer(eachText)
@@ -82,59 +84,64 @@ class DataCLean:
 
     def remove_punc(self, temp_df):
         count = 0
-        for text in temp_df['text']:
-            out = re.sub(r'[^\w\s]', '', text)
-            temp_df.at[count,'text'] = out
-            temp_df.at[count, 'class'] = temp_df.iloc[count]['class']
+        for text in temp_df["text"]:
+            out = re.sub(r"[^\w\s]", "", text)
+            temp_df.at[count, "text"] = out
+            temp_df.at[count, "class"] = temp_df.iloc[count]["class"]
             count += 1
         return temp_df
 
     def space(self, final_df):
         new_df = pd.DataFrame()
         count_tweets = 0
-        for text in final_df['text']:
+        for text in final_df["text"]:
             temp = ""
             for char in text:
-                if char in [",",".","!","?",":",";"]:
-                    temp = temp + ' ' + char
+                if char in [",", ".", "!", "?", ":", ";"]:
+                    temp = temp + " " + char
 
                 else:
                     temp = temp + char
             # print(temp)
-            new_df.at[count_tweets, 'text'] = temp
-            new_df.at[count_tweets, 'class'] = final_df.iloc[count_tweets]['class']
+            new_df.at[count_tweets, "text"] = temp
+            new_df.at[count_tweets, "class"] = final_df.iloc[count_tweets]["class"]
             count_tweets += 1
         # print("new_df")
         # print(new_df)
         return new_df
 
     def handle_negation(self, final_df):
-            out_df = pd.DataFrame()
-            count_tweet = 0
-            for text in final_df['text']:
-                temp_text = ""
-                li_text = text.split()
-                for word in li_text:
-                    count = 0
-                    lower_word = word.lower()
-                    if lower_word == "didn't" or lower_word == "not" or lower_word == "no" or lower_word == "never"\
-                            or lower_word == "don't":
-                        temp = count + 1
-                        temp_text = temp_text + word + " "
-                        for i in range(temp,len(li_text)):
-                            if li_text[i] in [",","?","!","."]:
-                                temp_text = " "+temp_text + li_text[i] + " "
-                                break
-                            else:
-                                temp_text = temp_text + "NOT_" + li_text[i]+" "
+        out_df = pd.DataFrame()
+        count_tweet = 0
+        for text in final_df["text"]:
+            temp_text = ""
+            li_text = text.split()
+            for word in li_text:
+                count = 0
+                lower_word = word.lower()
+                if (
+                    lower_word == "didn't"
+                    or lower_word == "not"
+                    or lower_word == "no"
+                    or lower_word == "never"
+                    or lower_word == "don't"
+                ):
+                    temp = count + 1
+                    temp_text = temp_text + word + " "
+                    for i in range(temp, len(li_text)):
+                        if li_text[i] in [",", "?", "!", "."]:
+                            temp_text = " " + temp_text + li_text[i] + " "
+                            break
+                        else:
+                            temp_text = temp_text + "NOT_" + li_text[i] + " "
 
-                    else:
-                        temp_text = temp_text + word + " "
-                # print(temp_text)
-                out_df.at[count_tweet, 'text'] = temp_text
-                out_df.at[count_tweet,'class'] = final_df.iloc[count_tweet]['class']
-                count_tweet += 1
-            return out_df
+                else:
+                    temp_text = temp_text + word + " "
+            # print(temp_text)
+            out_df.at[count_tweet, "text"] = temp_text
+            out_df.at[count_tweet, "class"] = final_df.iloc[count_tweet]["class"]
+            count_tweet += 1
+        return out_df
 
     def clean_data(self, final_df):
         # print(final_df)
@@ -151,13 +158,13 @@ class DataCLean:
         return li_remove_stopWords
 
     def DocVector(self, final_df, uniqueWords):
-        data = np.zeros([final_df['class'].count(), len(uniqueWords)])
+        data = np.zeros([final_df["class"].count(), len(uniqueWords)])
         docVector1 = pd.DataFrame(data, columns=uniqueWords)
-        docVector = docVector1.assign(PurchaseIntention=list(final_df['class']))
+        docVector = docVector1.assign(PurchaseIntention=list(final_df["class"]))
         # docVector['Purchase Intention'] = final_df['class']
         # print(docVector['PurchaseIntention'])
         doc_count = 0
-        for doc in final_df['text']:
+        for doc in final_df["text"]:
             words = doc.split()
             for word in words:
                 temp = word.lower()
@@ -167,15 +174,14 @@ class DataCLean:
 
         return docVector
 
-    
     def binary_docvector(self, final_df, uniqueWords):
-        data = np.zeros([final_df['class'].count(), len(uniqueWords)])
+        data = np.zeros([final_df["class"].count(), len(uniqueWords)])
         docVector1 = pd.DataFrame(data, columns=uniqueWords)
-        docVector = docVector1.assign(PurchaseIntention=list(final_df['class']))
+        docVector = docVector1.assign(PurchaseIntention=list(final_df["class"]))
         # docVector['Purchase Intention'] = final_df['class']
         # print(docVector['PurchaseIntention'])
         doc_count = 0
-        for doc in final_df['text']:
+        for doc in final_df["text"]:
             words = doc.split()
             for word in words:
                 temp = word.lower()
@@ -190,7 +196,7 @@ class DataCLean:
         unique = list(set(corpus.split()))
         # print(unique)
         # tfIdf_df = pd.DataFrame(columns=unique)
-        tf_df = self.DocVector(df_cleaned_text,unique)
+        tf_df = self.DocVector(df_cleaned_text, unique)
         # idf_df = pd.DataFrame()
         total_docs = len(tf_df.index)
         for column in unique:
@@ -203,15 +209,14 @@ class DataCLean:
             tf_df[column] = tf_df[column].multiply(idf)
             idf = 0
         # print(tf_df['buy'])
-        return tf_df        
-    
+        return tf_df
 
     def make_unique_li(self, li_cleanText):
         unique_words_set = set(li_cleanText)
         unique_word_li = list(unique_words_set)
         return unique_word_li
 
-    def stemmed(self,li_cleanText):
+    def stemmed(self, li_cleanText):
         count_stemed = 0
         for word in li_cleanText:
             if word[-1] == "s":
@@ -221,4 +226,5 @@ class DataCLean:
             elif word[-3:] == "ing":
                 li_cleanText[count_stemed] = word[:-3]
             count_stemed += 1
-        return li_cleanText 
+        return li_cleanText
+
--- models/SVMExperiment.py
+++ models/SVMExperiment.py
@@ -1,9 +1,26 @@
 import pandas as pd
 import Clean as cl
 from sklearn import svm
-#from sklearn.cross_validation import train_test_split
+
+# from sklearn.cross_validation import train_test_split
 from sklearn import model_selection, naive_bayes
 import numpy as np
+
+from sklearn.metrics import make_scorer, accuracy_score, f1_score
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import (
+    confusion_matrix,
+    roc_auc_score,
+    recall_score,
+    precision_score,
+)
+from sklearn.metrics import confusion_matrix
+
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# SVM
+
 
 def SVM(path):
     _dcl = cl.DataCLean()
@@ -12,63 +29,108 @@ def SVM(path):
     li_clean_text = _dcl.clean_data(final_df)
     uniqueWords = _dcl.make_unique_li(li_clean_text)
     # print(uniqueWords)
-    docVector = _dcl.DocVector(final_df, uniqueWords) ###_dcl.DocVector or _dcl.binary_docvectir
+    docVector = _dcl.DocVector(
+        final_df, uniqueWords
+    )  ###_dcl.DocVector or _dcl.binary_docvectir
     ###########################
     df = docVector.values
-    X_train,Y = df[:,:-1],df[:,-1]
-    Y_train = convert_to_0_or_1(Y);
+    X_train, Y = df[:, :-1], df[:, -1]
+    Y_train = convert_to_0_or_1(Y)
     return (X_train, Y_train)
-"""
-###Merge file function
-def MergeFile():
-    filenames = ['Training.csv', 'Testing.csv']
-    with open('MergeFile.csv', 'w',encoding='latin') as outfile:
-        for fname in filenames:
-            with open(fname,encoding='latin') as infile:
-                outfile.write(infile.read())
 
-#MergeFile()
-"""
+
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
 
 def convert_to_0_or_1(Y):
     Y_train = []
     for y in Y:
-        if y == 'yes':
+        if y == "yes":
             Y_train.append(1)
         else:
             Y_train.append(0)
     return Y_train
 
 
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# Training
 
-##Training
-
-path = '../data/AnnotatedData3.csv'
+# path = "../data/AnnotatedData3.csv"
+path = "data/AnnotatedData3.csv"
 X, Y = SVM(path)
 
-a = np.size(X,0)
-X_split = int(np.size(X,0)*0.7)
+a = np.size(X, 0)
+X_split = int(np.size(X, 0) * 0.7)
 
 
 X_train = X[0:X_split]
 Y_train = Y[0:X_split]
 
-X_test = X[X_split:,: ]
-Y_test = Y[X_split : ]
+X_test = X[X_split:, :]
+Y_test = Y[X_split:]
 
 
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
-#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+class_weight = {0: 2, 1: 1}
 
-class_weight = {0: 2,
-                1: 1,}
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# Applying SVM
 
-#print(Y_train)
-clf = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+# print(Y_train)
+clf = svm.SVC(probability=True, C=1.0, kernel="linear", degree=3, gamma="auto")
 model = clf.fit(X_train, Y_train)
-print(model.score(X_test,Y_test))
+print(model.score(X_test, Y_test))
+
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# Applying Naive Bayes
 
 Naive = naive_bayes.MultinomialNB()
 Naive.fit(X_train, Y_train)
-print(Naive.score(X_test,Y_test))
+print(Naive.score(X_test, Y_test))
 
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# statitics
+
+
+def report_results(model, X, y):
+    pred_proba = model.predict_proba(X)[:, 1]
+    pred = model.predict(X)
+
+    auc = roc_auc_score(y, pred_proba)
+    acc = accuracy_score(y, pred)
+    f1 = f1_score(y, pred)
+    prec = precision_score(y, pred)
+    rec = recall_score(y, pred)
+    tn, fp, fn, tp = confusion_matrix(y, pred).ravel()
+    result = {
+        "auc": auc,
+        "f1": f1,
+        "acc": acc,
+        "precision": prec,
+        "recall": rec,
+        "TN": tn,
+        "FP": fp,
+        "FN": fn,
+        "TP": tp,
+    }
+    return result
+
+
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+# statitics for SVM
+
+stats = report_results(model, X_test, Y_test)
+print(stats)
