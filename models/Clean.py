adc5d7a408a293599a938602ca4765d758ee6fc7
MuhammadAmmarSaleem <34312061+MuhammadAmmarSaleem@users.noreply.github.com>
2019-04-13 23:50:24 +0500

    Added new clean steps

--- models/Clean.py
+++ models/Clean.py
@@ -1,27 +1,22 @@
 import pandas as pd
-import numpy as np
-import nltk
+# import numpy as np
+# import nltk
 import re
-import pathConfig as pc #import path of stopwords
-
 # from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
 # import math
-
-
-#--------------------
-pathStopWords = pc.PATH_CONFIG['pathStopWords']
-#-------------------
-
+import enchant
+from stemming.porter2 import stem
+import pathConfig as pc
 class DataCLean:
+
     def extract(self, path):
-        fd = open(path, encoding="utf-8", errors="replace")
+        fd = open(path, encoding="utf-8", errors='replace')
         df = pd.read_csv(fd)
-        defined = df["class"] != ("undefined")
+        defined = df['class'] != ("undefined")
         # #output dataframe without undeined
         df2 = df[defined]
-        defined1 = df2["class"] != "Undefined"
+        defined1 = df2['class'] != "Undefined"
         df4 = df2[defined1]
         # replace no PI with no
         df3 = df4.replace("No PI", "no")
@@ -45,10 +40,10 @@ class DataCLean:
         li_stopwords = stopword[0].split()
         return li_stopwords
 
-    def removeStopWords(self, text):
+
+    def removeStopWords(self,text):
         # stop_words = set(stopwords.words('english'))
-        # stop_words = self.read_stopwords("stopwords.txt")
-        stop_words = self.read_stopwords(pathStopWords)  # ubuntu/linux
+        stop_words = self.read_stopwords("E:/DATA/Sem8/fyp/stopwords.txt")
         word_tokens = word_tokenize(text)
         filtered_sentence = []
         for w in word_tokens:
@@ -59,26 +54,24 @@ class DataCLean:
 
     def remove_stopwords(self, df_punc_remove):
         # stop_words = set(stopwords.words('english'))
-        li_stopwords = self.read_stopwords(pathStopWords)
+        li_stopwords = self.read_stopwords("E:/DATA/Sem8/fyp/stopwords.txt")
         # print(stop_words)
         count_clean = 0
-        for text in df_punc_remove["text"]:
+        for text in df_punc_remove['text']:
             word_tokens = word_tokenize(text)
             clean_text = ""
             for w in word_tokens:
                 if w.lower() not in li_stopwords:
-                    clean_text = clean_text + w.lower() + " "
-            df_punc_remove.at[count_clean, "text"] = clean_text
-            df_punc_remove.at[count_clean, "class"] = df_punc_remove.iloc[count_clean][
-                "class"
-            ]
+                    clean_text = clean_text + w.lower() + ' '
+            df_punc_remove.at[count_clean, 'text'] = clean_text
+            df_punc_remove.at[count_clean, 'class'] = df_punc_remove.iloc[count_clean]['class']
             count_clean += 1
         # return list of corpus without stop words in a list.
         # print(df_punc_remove)
         return df_punc_remove
 
     def removePunc(self, eachText):
-        remove_punc = re.sub(r"[^\w\s]", "", eachText)
+        remove_punc = re.sub(r'[^\w\s]', '', eachText)
         return remove_punc
         # pattern = re.compile(r'[a-zA-Z]+')
         # matches = pattern.finditer(eachText)
@@ -89,139 +82,117 @@ class DataCLean:
 
     def remove_punc(self, temp_df):
         count = 0
-        for text in temp_df["text"]:
-            out = re.sub(r"[^\w\s]", "", text)
-            temp_df.at[count, "text"] = out
-            temp_df.at[count, "class"] = temp_df.iloc[count]["class"]
+        for text in temp_df['text']:
+            out = re.sub(r'[^\w\s]', '', text)
+            temp_df.at[count,'text'] = out
+            temp_df.at[count, 'class'] = temp_df.iloc[count]['class']
             count += 1
         return temp_df
 
     def space(self, final_df):
         new_df = pd.DataFrame()
         count_tweets = 0
-        for text in final_df["text"]:
+        for text in final_df['text']:
             temp = ""
             for char in text:
-                if char in [",", ".", "!", "?", ":", ";"]:
-                    temp = temp + " " + char
+                if char in [",",".","!","?",":",";"]:
+                    temp = temp + ' ' + char
 
                 else:
                     temp = temp + char
             # print(temp)
-            new_df.at[count_tweets, "text"] = temp
-            new_df.at[count_tweets, "class"] = final_df.iloc[count_tweets]["class"]
+            new_df.at[count_tweets, 'text'] = temp
+            new_df.at[count_tweets, 'class'] = final_df.iloc[count_tweets]['class']
             count_tweets += 1
         # print("new_df")
         # print(new_df)
         return new_df
 
     def handle_negation(self, final_df):
-        out_df = pd.DataFrame()
-        count_tweet = 0
-        for text in final_df["text"]:
-            temp_text = ""
-            li_text = text.split()
-            for word in li_text:
-                count = 0
-                lower_word = word.lower()
-                if (
-                    lower_word == "didn't"
-                    or lower_word == "not"
-                    or lower_word == "no"
-                    or lower_word == "never"
-                    or lower_word == "don't"
-                ):
-                    temp = count + 1
-                    temp_text = temp_text + word + " "
-                    for i in range(temp, len(li_text)):
-                        if li_text[i] in [",", "?", "!", "."]:
-                            temp_text = " " + temp_text + li_text[i] + " "
-                            break
-                        else:
-                            temp_text = temp_text + "NOT_" + li_text[i] + " "
+            out_df = pd.DataFrame()
+            count_tweet = 0
+            for text in final_df['text']:
+                temp_text = ""
+                li_text = text.split()
+                for word in li_text:
+                    count = 0
+                    lower_word = word.lower()
+                    if lower_word == "didn't" or lower_word == "not" or lower_word == "no" or lower_word == "never"\
+                            or lower_word == "don't":
+                        temp = count + 1
+                        temp_text = temp_text + word + " "
+                        for i in range(temp,len(li_text)):
+                            if li_text[i] in [",","?","!","."]:
+                                temp_text = " "+temp_text + li_text[i] + " "
+                                break
+                            else:
+                                temp_text = temp_text + "NOT_" + li_text[i]+" "
 
-                else:
-                    temp_text = temp_text + word + " "
-            # print(temp_text)
-            out_df.at[count_tweet, "text"] = temp_text
-            out_df.at[count_tweet, "class"] = final_df.iloc[count_tweet]["class"]
-            count_tweet += 1
-        return out_df
+                    else:
+                        temp_text = temp_text + word + " "
+                # print(temp_text)
+                out_df.at[count_tweet, 'text'] = temp_text
+                out_df.at[count_tweet,'class'] = final_df.iloc[count_tweet]['class']
+                count_tweet += 1
+            return out_df
+
+    def check_english(self, temp_df):
+        d = enchant.Dict("en_US")
+        new_eng = pd.DataFrame()
+        count = 0
+
+        for sentence in temp_df['text']:
+            temp_sent = ""
+            for word in sentence.split():
+                temp = word.lower()
+                if d.check(word):
+                    # print("ammar")
+                    temp_sent = temp_sent + temp + " "
+            # print(temp_sent)
+            new_eng.at[count,'text'] = temp_sent
+            new_eng.at[count, 'class'] = temp_df.iloc[count]['class']
+            count += 1
+        # print(new_eng)
+        return new_eng
+
+    def Stemming(self, temp_df):
+        new_eng = pd.DataFrame()
+        count = 0
+
+        for sentence in temp_df['text']:
+            temp_sent = ""
+            for word in sentence.split():
+                temp = stem(word.lower())
+                temp_sent = temp_sent + temp + " "
+            # print(temp_sent)
+            new_eng.at[count, 'text'] = temp_sent
+            new_eng.at[count, 'class'] = temp_df.iloc[count]['class']
+            count += 1
+        # print(new_eng)
+        return new_eng
+
+        # stem("factionally")
 
     def clean_data(self, final_df):
         # print(final_df)
-        new_df = self.space(final_df)
-        # print("Hello clean data")
-        # print(new_df["text"])
-        new_corpus_df = self.handle_negation(new_df)
-        # print(new_corpus_df['text'])
-        new_corpus = self.text_concat(new_corpus_df)
-
-        remove_punc = self.removePunc(new_corpus)
-        li_remove_stopWords = self.removeStopWords(remove_punc)
-        # print(li_remove_stopWords)
-        return li_remove_stopWords
-
-    def DocVector(self, final_df, uniqueWords):
-        data = np.zeros([final_df["class"].count(), len(uniqueWords)])
-        docVector1 = pd.DataFrame(data, columns=uniqueWords)
-        docVector = docVector1.assign(PurchaseIntention=list(final_df["class"]))
-        # docVector['Purchase Intention'] = final_df['class']
-        # print(docVector['PurchaseIntention'])
-        doc_count = 0
-        for doc in final_df["text"]:
-            words = doc.split()
-            for word in words:
-                temp = word.lower()
-                if temp in docVector.columns:
-                    docVector.at[doc_count, temp] += 1
-            doc_count += 1
-
-        return docVector
-
-    def binary_docvector(self, final_df, uniqueWords):
-        data = np.zeros([final_df["class"].count(), len(uniqueWords)])
-        docVector1 = pd.DataFrame(data, columns=uniqueWords)
-        docVector = docVector1.assign(PurchaseIntention=list(final_df["class"]))
-        # docVector['Purchase Intention'] = final_df['class']
-        # print(docVector['PurchaseIntention'])
-        doc_count = 0
-        for doc in final_df["text"]:
-            words = doc.split()
-            for word in words:
-                temp = word.lower()
-                if temp in docVector.columns:
-                    if docVector.iloc[doc_count][temp] < 1:
-                        docVector.at[doc_count, temp] += 1
-            doc_count += 1
-        # print(docVector['good'])
-        return docVector
-
-    def tf_idf(self, corpus, df_cleaned_text):
-        unique = list(set(corpus.split()))
-        # print(unique)
-        # tfIdf_df = pd.DataFrame(columns=unique)
-        tf_df = self.DocVector(df_cleaned_text, unique)
-        # idf_df = pd.DataFrame()
-        total_docs = len(tf_df.index)
-        for column in unique:
-            num_doc_word = 0
-            for no_doc in range(total_docs):
-                if tf_df.at[no_doc, column] != 0:
-                    num_doc_word += 1
-            idf = math.log(total_docs / num_doc_word)
-            # idf_df.at[0,column] = idf
-            tf_df[column] = tf_df[column].multiply(idf)
-            idf = 0
-        # print(tf_df['buy'])
-        return tf_df
+        eng_df = self.check_english(final_df)
+        # print(eng_df)
+        df_stem = self.Stemming(eng_df)
+        new_df = self.space(df_stem)
+        df_remove_stopWords = self.remove_stopwords(new_df)
+        new_corpus_df = self.handle_negation(df_remove_stopWords)
+        remove_punc_df = self.remove_punc(new_corpus_df)
+        new_corpus = self.text_concat(remove_punc_df)
+        li_new_corpus = new_corpus.split()
+        print(remove_punc_df)
+        return li_new_corpus, remove_punc_df
 
     def make_unique_li(self, li_cleanText):
         unique_words_set = set(li_cleanText)
         unique_word_li = list(unique_words_set)
         return unique_word_li
 
-    def stemmed(self, li_cleanText):
+    def stemmed(self,li_cleanText):
         count_stemed = 0
         for word in li_cleanText:
             if word[-1] == "s":
@@ -232,4 +203,3 @@ class DataCLean:
                 li_cleanText[count_stemed] = word[:-3]
             count_stemed += 1
         return li_cleanText
-
