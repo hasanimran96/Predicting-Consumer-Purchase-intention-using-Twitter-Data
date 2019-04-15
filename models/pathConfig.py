0e258c784651d94cd2680fd29afae4d7c45a7695
Saad Salman <saadsalman486@gmail.com>
2019-04-15 13:49:20 +0500

    Added sklearn neural network

--- models/pathConfig.py
+++ models/pathConfig.py
@@ -1,6 +1,6 @@
 PATH_CONFIG = {
-    #'pathData' : '..\data\AnnotatedData3.csv', #Windows data path
-    #'pathStopWords' : 'stopwords.txt',          #Windows stopwords path
-    "pathData": "data/AnnotatedData3.csv",  # Ubuntu data path
-    "pathStopWords": "models/stopwords.txt",  # Ubuntu stopwords path
+    'pathData' : '..\data\AnnotatedData3.csv', #Windows data path
+    'pathStopWords' : 'stopwords.txt',          #Windows stopwords path
+    #"pathData": "data/AnnotatedData3.csv",  # Ubuntu data path
+    #"pathStopWords": "models/stopwords.txt",  # Ubuntu stopwords path
 }
