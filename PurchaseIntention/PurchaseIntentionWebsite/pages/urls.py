da8857a2d748e403d88cfba1e5a3b45a4d77604f
Ammar Saleem <ammarsaleem1995@gmail.com>
2019-04-19 11:37:45 +0500

    added file upload in views

--- PurchaseIntention/PurchaseIntentionWebsite/pages/urls.py
+++ PurchaseIntention/PurchaseIntentionWebsite/pages/urls.py
@@ -3,7 +3,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('upload/', views.upload, name='upload'),
+    path('annotate/', views.upload_annotate, name='annotate'),
+    path('test/', views.upload_test, name='test'),
     path('analysis/', views.analysis, name='analysis'),
     path('result/', views.result, name='result'),
 ]
--- PurchaseIntention/PurchaseIntentionWebsite/pages/views.py
+++ PurchaseIntention/PurchaseIntentionWebsite/pages/views.py
@@ -1,31 +1,73 @@
 from django.shortcuts import render
+from django.contrib import messages
 import csv, io
 def home(request):
     return render(request, "home.html", {})
 
 def dashboard(request):
     return render(request, "dashboard.html", {})
-
-def upload(request):
-    template = "upload.html"
-    if request.method == "GET":
-        return render(request, template,{})
+   
+def upload_annotate(request):
+        template = "annotate.html"
+        if request.method == "GET":
+                return render(request, template,{})
      
-    csv_file = request.FILES['file']
+        csv_file = request.FILES['file']
+        filename = csv_file.name
+
+        if not csv_file.name.endswith('.csv'):
+                messages.error(request,"This is not csv file")
+                err = 1
+                return render(request, template, {"error":err})
+                
+        if csv_file.name.endswith('.csv'):
+                dataset = csv_file.read().decode('UTF-8')
+                io_string = io.StringIO(dataset)
+
+                # with open('Data.csv', newline='', encoding='utf-8-sig') as csvFile:
+                # for line in csv.reader(csvFile, delimiter=';'):
+
+
+                with open('uploadeddata/'+filename, 'w', newline='', encoding='utf-8-sig') as csvfile:
+                        filewriter = csv.writer(csvfile, delimiter=',',
+                                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                        for row in csv.reader(io_string, delimiter=',',quotechar="|"):
+                                filewriter.writerow(row)
+
+                copied = 1            
+                return render(request, template, {"success":copied})
+
+
+def upload_test(request):
+        template = "test.html"
+        if request.method == "GET":
+                return render(request, template,{})
      
-    if not csv_file.name.endswith('.csv'):
-         messages.error(request,"This is not csv file")
+        csv_file = request.FILES['file']
+        filename = csv_file.name
 
-    dataset = csv_file.read().decode('UTF-8')
-    io_string = io.StringIO(dataset)
-    next(io_string)
+        if not csv_file.name.endswith('.csv'):
+                messages.error(request,"This is not csv file")
+                err = 1
+                return render(request, template, {"error":err})
+                
+        if csv_file.name.endswith('.csv'):
+                dataset = csv_file.read().decode('UTF-8')
+                io_string = io.StringIO(dataset)
 
-    with open('uploadeddata/upload1.csv', 'w') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for row in csv.reader(io_string, delimiter=',',quotechar="|"):
-            filewriter.writerow(row)
-    return render(request, template, {})
+                # with open('Data.csv', newline='', encoding='utf-8-sig') as csvFile:
+                # for line in csv.reader(csvFile, delimiter=';'):
+
+
+                with open('uploadeddata/'+filename, 'w', newline='', encoding='utf-8-sig') as csvfile:
+                        filewriter = csv.writer(csvfile, delimiter=',',
+                                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                        for row in csv.reader(io_string, delimiter=',',quotechar="|"):
+                                filewriter.writerow(row)
+
+                copied = 1            
+                return render(request, template, {"success":copied})
+
 
 def analysis(request):
     return render(request, "analysis.html", {})
